0e0e06a5b92e450f020932e8cdf6675a0c43edcf
Umarjon <sharofiddinov.07@mail.ru>
2021-10-26 16:33:41 +0300

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar/index'
 import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>What's upppp</h1>
+      <Navbar/>
     </div>
   );
 }
